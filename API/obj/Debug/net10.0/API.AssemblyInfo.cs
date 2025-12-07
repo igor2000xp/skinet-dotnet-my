c35cfe14e427a2b7ba124af42af474575a4af38a
igor2000xp <igorring@google.com>
2025-12-07 18:12:38 +0100

    chore: Remove bin and obj folders from git tracking

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4def0ac16b749004e2f733c68571cbc358634614")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40011df2c14f169df3d4eb7ab977c6e9cb4e3b55")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
